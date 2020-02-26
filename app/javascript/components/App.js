a870669941b8c855a39bbb8ad1f1b641078f4638
brad-bartlett <bbartlett222@gmail.com>
2020-02-26 15:13:55 +1100

    routes set up and firing

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Route, Switch} from 'react-router-dom'
 import Home from './Home'
+import About from './About'
 
 class App extends React.Component {
     render() {
@@ -8,7 +9,7 @@ class App extends React.Component {
             <div> 
                 <Switch>
                     <Route exact path = '/' component={Home}/>
-                    {/* <Route exact path='/about' component={About}/> */}
+                    <Route exact path='/about' component={About} />
                 </Switch>
             </div>
         )
--- app/javascript/packs/Index.js
+++ app/javascript/packs/Index.js
@@ -4,10 +4,8 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from '../components/App'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
-
-
+import App from '../components/App'
 
 
 document.addEventListener('DOMContentLoaded', () => {
